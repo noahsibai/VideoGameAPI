fbc85cc8738a3e04a2b96d7446c78bd367af222e
noahsibai <sibai1nk@outlook.com>
2019-05-01 22:55:25 -0400

    Firebase integration

--- sorter.py
+++ sorter.py
@@ -3,47 +3,72 @@
 #
 # @author: Noah Sibai
 # Wrote: April 30, 2019
-# 
+#
 
-video_games = [["The Adventure Pals", 10 , False, "Platformer"], ["Attack On Titan 2", 17, False, "Action-Adventure"], ["Bendy and the Ink Machine", 6, False, "Horor"], ["Crash Bandicoot N.Sane Trilogy", 14.5, False, "Platformer"], ["Darksiders Warmastered Edition", 17, False, "Action-Adventure"], ["De Blob", 8, False, "Platform-Puzzle"], ["De Blob 2", 11, False, "Platform-Puzzle"], ["Diablo III: Eternal Collection", 20.5, False, "Action RPG"], ["DragonBall Xenoverse 2", 19, False, "Fighting Game"], ["Dragon's Dogma: Dark Aisen", 37, False, "RPG"], ["Monster Boy and the Cursed Kingdom", 18.5, True, "Platformer"]]
+from firebase import firebase
+import json
+import sys
+
+firebase = firebase.FirebaseApplication("https://videogamelist-5ed90.firebaseio.com", None)
+video_games = []
 not_completed = []
 completed = []
 
 def main():
+    # while True:
+    #     firebase.post("/games",add_new())
+
+    result = firebase.get("/", None)
+    for key,val in result["games"].items():
+        video_games.append([val["title"], val["time"], val["genre"], val["completed"]])
+    
+    # print (video_games)
     sort()
     split()
     output()
+
+def add_new():
+    title = input("Title: ")
+    time = float(input("Time: "))
+    genre = input("Genre: ")
+    completed = input("Competed True or False: ")
+    end = input("end: ")
+    if end == "y":
+        sys.exit()
+    return {"title":title, "time":time, "genre":genre, "completed":completed}
 
 def sort():
     # Determine sort method
     try:
         sort_type = int(input("0:Name 1:Time 4:Genre "))
         if (sort_type == 0 or sort_type == 1 or sort_type == 4):
-            video_games.sort(key=lambda game: (game[sort_type], game[1]))
+            video_games.sort(key=lambda game: (game[sort_type]))
         else:
             video_games.sort(key=lambda game: game[1])
     except:
         video_games.sort(key=lambda game: game[1])
 
+
 def split():
     # Splits list based on Completetion Status
     for game in video_games:
-        if game[2]:
+        if game[3] == "True":
             completed.append(game)
         else:
             not_completed.append(game)
 
+
 def output():
     # Print output of the sort to the console
-    print ("Not Completed")
+    print("Not Completed")
     for game in not_completed:
-        # print (game[0] + " " + str(game[1]) + " " + game[3])
-        cur_game = "{0} | {1} hours | {2}".format(game[0],game[1],game[3])
-        print (cur_game)
+        cur_game = "{0} | {1} hours | {2}".format(game[0], game[1], game[2])
+        print(cur_game)
 
-    print ("\nCompleted")
+    print("\nCompleted")
     for game in completed:
-        print (game[0])
+        print(game[0])
+
 
 if __name__ == "__main__":
     main()
